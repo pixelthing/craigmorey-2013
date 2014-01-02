be6493401e1347ee4a7f2ef6ef09f029ec2ff0ae
Craig Morey <craig.morey@uswitch.com>
2014-01-02 17:36:37 +0000

    refector jquery append->inner html

--- source/javascripts/_blog.js
+++ source/javascripts/_blog.js
@@ -15,16 +15,16 @@ cmJs.blogFeed = (function() {
   },
 
   formatFeed = function(feed) { 
-    var html = '';
+    var buffer = '';
     var maxItems = 3;
     maxItems = (maxItems > feed.items.length ? feed.items.length : maxItems );
     for(var i = 0; i < maxItems; i++) {
       var item = feed.items[i];
       var dateBreak = item.updated.indexOf(' ',14);
       var itemDate = item.updated.substring(0,dateBreak);
-      html += '<article><a href="' + item.link + '"><h3>' + itemDate + '</h3><h2>' + item.title + '</h2><p>' + item.description + '</p></a></article>';
+      buffer += '<article><a href="' + item.link + '"><h3>' + itemDate + '</h3><h2>' + item.title + '</h2><p>' + item.description + '</p></a></article>';
     }
-    $('.posts').append(html);
+    $('.posts').html(buffer);
   }
 
   return {
