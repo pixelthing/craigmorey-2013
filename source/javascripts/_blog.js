5b3e6531abe8efd28f1f26c174139308d4f2ab86
Craig Morey <craig.morey@uswitch.com>
2014-01-02 15:22:59 +0000

    add blog feed retrieval

---
+++ source/javascripts/_blog.js
@@ -0,0 +1,36 @@
+var cmJs = cmJs || {};
+
+cmJs.blogFeed = (function() {
+
+  var init = function () {
+    fetchFeed();
+  },
+
+  fetchFeed = function() {
+    var feedUrl = '/blog/?feed=rss2';
+    jQuery.getFeed({
+      url: feedUrl,
+      success: formatFeed 
+    });
+  },
+
+  formatFeed = function(feed) { 
+    var html = '';
+    var maxItems = 3;
+    maxItems = (maxItems > feed.items.length ? feed.items.length : maxItems );
+    for(var i = 0; i < maxItems; i++) {
+      var item = feed.items[i];
+      var dateBreak = item.updated.indexOf(' ',14);
+      var itemDate = item.updated.substring(0,dateBreak);
+      html += '<article><a href="' + item.link + '"><h3>' + itemDate + '</h3><h2>' + item.title + '</h2><p>' + item.description + '</p></a></article>';
+    }
+    $('.posts').append(html);
+  }
+
+  return {
+    init: function() { $(init); }
+  };
+
+}(jQuery));
+
+cmJs.blogFeed.init();
