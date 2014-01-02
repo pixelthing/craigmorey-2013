f6fab74b2e30f69cf285dd22cb0e5bcfb79ccd1c
Craig Morey <craig.morey@uswitch.com>
2014-01-02 17:35:35 +0000

    add codepen feed, format it up

--- source/javascripts/_codepen.js
+++ source/javascripts/_codepen.js
@@ -14,7 +14,26 @@ cmJs.codepen = (function() {
         url: 'http://codepen-awesomepi.timpietrusky.com/pixelthing/?callback=jsonp'
       })
       .success(function(data){
-        console.log( data )
+        var pensArray = data.content.pens;
+        var buffer = '';
+        var maxItems = 4;
+        maxItems = (maxItems > pensArray.length ? pensArray.length : maxItems );
+        for(var i = 0; i < maxItems; i++) {
+          var item = pensArray[i];
+          buffer += '<article>';
+          buffer += '<a href="' + item.url.pen + '" target="_blank"><h2>' + item.title + '</h2><p>' + item.description + '</p><dl>';
+          if (parseInt(item.views) > 0) {
+            buffer += '<dt class="pen-views">Views: </dt><dd>' + item.views + '</dd>'
+          }
+          if (parseInt(item.loves) > 0) {
+            buffer += '<dt class="pen-loves">Loves: </dt><dd>' + item.loves + '</dd>'
+          }
+          if (parseInt(item.comments) > 0) {
+            buffer += '<dt class="pen-comments">Comments:: </dt><dd>' + item.comments + '</dd>'
+          }
+          buffer += '</dl></a></article>';
+        }
+        $('.pens').html(buffer);
       })
 
   }
