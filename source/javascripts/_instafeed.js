9a7b86b21e15ba8b39cc47dc85e61a6fd9fb48e9
Craig Morey <craig.morey@uswitch.com>
2014-01-02 15:21:42 +0000

    instagram stuff

--- source/javascripts/_instafeed.js
+++ source/javascripts/_instafeed.js
@@ -14,10 +14,30 @@ cmJs.instafeed = (function() {
       resolution: 'low_resolution',
       limit: 9,
       accessToken: '62527.467ede5.73ac26e776c149ff9f931d0534159963',
-      template: '<figure><a href="{{link}}"><img src="{{image}}" /><figcaption>{{caption}} {{location}}</figcaption></a></figure>'
+      template: '<figure data-type="{{model.type}}" data-likes="{{likes}}" data-image="{{id}}" data-video="{{model.videos.standard_resolution.url}}" data-hires="{{model.images.standard_resolution.url}}" data-created="{{model.created_time}}"><a href="{{link}}"><img src="{{image}}" /><figcaption>{{caption}} <span class="figure-location">{{location}}</figcaption></a></figure>',
+      error: function(){
+        alert('oops, instagram isn\'t playing nice today.');
+      },
+      after: pictureInit
     });
     feed.run();
+  },
 
+  pictureInit = function() {
+    var pics = $('#instafeed figure');
+    $.each(pics,function(index,el){
+      var elJQ = $(el)
+      var type = elJQ.data('type');
+      var timestamp = elJQ.data('created');
+      var date = new Date(timestamp * 1000);
+      var dateReal = date.toUTCString();
+      var dateBreak = dateReal.indexOf(' ',14);
+      dateReal = dateReal.substring(0,dateBreak);
+      if (type=='video') {
+        elJQ.addClass('instagram-video');
+      }
+      elJQ.find('a').append('<div class="figure-date">' + dateReal + '</div>');
+    })
   }
 
   return {
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,5 @@
-//= require instafeed.js
-//= require _codepen.js
+//= require vendor/instafeed.js
+//= require vendor/jquery.jfeed.js
 //= require _instafeed.js
+//= require _blog.js
+//= require _codepen.js
