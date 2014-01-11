574ea8156fdb5c70422665b3d597b20d1a44162e
Craig Morey <craig.morey@uswitch.com>
2014-01-11 17:32:06 +0000

    Add error catching to codepen feed script

--- source/javascripts/_codepen.js
+++ source/javascripts/_codepen.js
@@ -8,30 +8,39 @@ cmJs.codepen = (function() {
 
   fetchJson = function() {
 
+    var buffer = '';
+    var maxItems = 4;
+
     $.ajax({
         dataType: 'jsonp',
         jsonp: 'jsonp',
         url: 'http://codepen-awesomepi.timpietrusky.com/pixelthing/?callback=jsonp'
       })
-      .success(function(data){
-        var pensArray = data.content.pens;
-        var buffer = '';
-        var maxItems = 4;
-        maxItems = (maxItems > pensArray.length ? pensArray.length : maxItems );
-        for(var i = 0; i < maxItems; i++) {
-          var item = pensArray[i];
-          buffer += '<article>';
-          buffer += '<a href="' + item.url.pen + '" target="_blank"><h2>' + item.title + '</h2><p>' + item.description + '</p><dl>';
-          if (parseInt(item.views) > 0) {
-            buffer += '<dt class="pen-views">Views: </dt><dd>' + item.views + '</dd>'
+      .error(function(data){
+        buffer = '<div class="error">Sorry - codepen could not be reached.</div>';
+        $('.pens').html(buffer);
+      }).success(function(data){
+        // invalid response
+        if (!data.content) {
+          buffer = '<div class="error">Sorry - codepen could not be reached.</div>'
+        } else {
+          var pensArray = data.content.pens;
+          maxItems = (maxItems > pensArray.length ? pensArray.length : maxItems );
+          for(var i = 0; i < maxItems; i++) {
+            var item = pensArray[i];
+            buffer += '<article>';
+            buffer += '<a href="' + item.url.pen + '" target="_blank"><h2>' + item.title + '</h2><p>' + item.description + '</p><dl>';
+            if (parseInt(item.views) > 0) {
+              buffer += '<dt class="pen-views">Views: </dt><dd>' + item.views + '</dd>'
+            }
+            if (parseInt(item.hearts) > 0) {
+              buffer += '<dt class="pen-loves">Hearts: </dt><dd>' + item.hearts + '</dd>'
+            }
+            if (parseInt(item.comments) > 0) {
+              buffer += '<dt class="pen-comments">Comments:: </dt><dd>' + item.comments + '</dd>'
+            }
+            buffer += '</dl></a></article>';
           }
-          if (parseInt(item.hearts) > 0) {
-            buffer += '<dt class="pen-loves">Hearts: </dt><dd>' + item.hearts + '</dd>'
-          }
-          if (parseInt(item.comments) > 0) {
-            buffer += '<dt class="pen-comments">Comments:: </dt><dd>' + item.comments + '</dd>'
-          }
-          buffer += '</dl></a></article>';
         }
         $('.pens').html(buffer);
       })
