612cd82b5eb5523ec684b08833d428d2fe3f3efa
Craig Morey <craig.morey@uswitch.com>
2014-01-12 02:43:47 +0000

    re-arrange the javascripts into sections

--- source/javascripts/all.js
+++
@@ -1,5 +0,0 @@
-//= require vendor/instafeed.js
-//= require vendor/jquery.jfeed.js
-//= require _instafeed.js
-//= require _blog.js
-//= require _codepen.js
---
+++ source/javascripts/home/all.js
@@ -0,0 +1,5 @@
+//= require vendor/instafeed.js
+//= require vendor/jquery.jfeed.js
+//= require home/_instafeed.js
+//= require home/_blog.js
+//= require home/_codepen.js
