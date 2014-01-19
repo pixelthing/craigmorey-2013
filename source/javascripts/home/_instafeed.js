a81d45b3689db6ca5aa1488f53b9a4b3a5520395
Craig Morey <craig.morey@uswitch.com>
2014-01-19 01:31:38 +0000

    add instagram pop-ups

--- source/javascripts/home/_instafeed.js
+++ source/javascripts/home/_instafeed.js
@@ -14,7 +14,7 @@ cmJs.instafeed = (function() {
       resolution: 'low_resolution',
       limit: 9,
       accessToken: '62527.467ede5.73ac26e776c149ff9f931d0534159963',
-      template: '<figure data-type="{{model.type}}" data-likes="{{likes}}" data-image="{{id}}" data-video="{{model.videos.standard_resolution.url}}" data-hires="{{model.images.standard_resolution.url}}" data-created="{{model.created_time}}"><a href="{{link}}"><img src="{{image}}" /><figcaption>{{caption}} <span class="figure-location">{{location}}</figcaption></a></figure>',
+      template: '<figure data-type="{{model.type}}" data-location="{{location}}" data-likes="{{likes}}" data-comments="{{comments}}" data-image="{{id}}" data-video="{{model.videos.standard_resolution.url}}" data-link="{{link}}" data-created="{{model.created_time}}"><a href="{{model.images.standard_resolution.url}}" class="fancybox" rel="instagram" title="{{caption}}"><img src="{{image}}" alt="Instagram" /></a></figure>',
       error: function(){
         alert('oops, instagram isn\'t playing nice today.');
       },
@@ -26,7 +26,8 @@ cmJs.instafeed = (function() {
   pictureInit = function() {
     var pics = $('#instafeed figure');
     $.each(pics,function(index,el){
-      var elJQ = $(el)
+      var elJQ = $(el);
+      var aJQ = elJQ.find('a');
       var type = elJQ.data('type');
       var timestamp = elJQ.data('created');
       var date = new Date(timestamp * 1000);
@@ -35,8 +36,9 @@ cmJs.instafeed = (function() {
       dateReal = dateReal.substring(0,dateBreak);
       if (type=='video') {
         elJQ.addClass('instagram-video');
+        aJQ.attr({'href':elJQ.data('video'),'data-fancybox-type':'iframe'})
       }
-      elJQ.find('a').append('<div class="figure-date">' + dateReal + '</div>');
+      aJQ.append('<div class="figure-date">' + dateReal + '</div>');
     })
   }
 
