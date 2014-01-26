d14e676a54049965b8661635ec753cb151515de9
Craig Morey <craig.morey@uswitch.com>
2014-01-26 15:33:54 +0000

    move home script into a closure

--- source/javascripts/home/all.js
+++ source/javascripts/home/all.js
@@ -6,22 +6,39 @@
 //= require home/_blog.js
 //= require home/_codepen.js
 
-$(".fancybox").fancybox({
-  afterLoad: function() {
-    var figure = $(this.element[0].parentNode);
-    var realDate = figure.find('.figure-date').text();
-    var location = figure.attr('data-location');
-    location = (location.length > 0 ? ' <span class="icon-location cm-icon"></span> ' + location : '');
-    var likes = figure.attr('data-likes');
-    likes = (parseInt(likes) > 0 ? ' <span class="icon-heart cm-icon"></span> ' + likes : '');
-    var comments = figure.attr('data-comments');
-    comments = (parseInt(likes) > 0 ? ' <span class="icon-bubble cm-icon"></span> ' + comments : '');
-    var link = figure.attr('data-link');
-    this.title = '<b>' + realDate + '</b> ' + this.title + '<br />' + location + likes + comments + ' <a href="' + link + '" target="_blank"><span class="icon-link cm-icon"></span> View on instagram</a>';
+var cmJs = cmJs || {};
+
+cmJs.home = (function() {
+  
+  var init = function() {
+    setupFancybox();
   },
-  helpers : {
-    title : {
-      type : 'inside'
-    }
+
+  setupFancybox = function() {
+    $(".fancybox").fancybox({
+      afterLoad: function() {
+        var figure = $(this.element[0].parentNode);
+        var realDate = figure.find('.figure-date').text();
+        var location = figure.attr('data-location');
+        location = (location.length > 0 ? ' <span class="icon-location cm-icon"></span> ' + location : '');
+        var likes = figure.attr('data-likes');
+        likes = (parseInt(likes) > 0 ? ' <span class="icon-heart cm-icon"></span> ' + likes : '');
+        var comments = figure.attr('data-comments');
+        comments = (parseInt(likes) > 0 ? ' <span class="icon-bubble cm-icon"></span> ' + comments : '');
+        var link = figure.attr('data-link');
+        this.title = '<b>' + realDate + '</b> ' + this.title + '<br />' + location + likes + comments + ' <a href="' + link + '" target="_blank"><span class="icon-link cm-icon"></span> View on instagram</a>';
+      },
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      }
+    });
   }
-});
+
+  $(init);
+
+  return {
+    init: function() { $(init); }
+  };
+}(jQuery));
