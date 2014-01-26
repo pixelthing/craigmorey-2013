60df9b7dd1045cd3d6c76a8b778ba0d663b2a9d9
Craig Morey <craig.morey@uswitch.com>
2014-01-26 14:55:08 +0000

    put work script in a closure

--- source/javascripts/work/all.js
+++ source/javascripts/work/all.js
@@ -1,22 +1,40 @@
+//= require vendor/keymaster.min.js
 //= require vendor/jquery.lazyload.min.js
 //= require vendor/jquery.fancybox.pack.js
 //= require vendor/jquery.fancybox-media.js
 //= require work/_work-nav.js
 
-$(".fancybox").fancybox({
-  helpers : {
-    title : {
-      type : 'inside'
-    }
+var cmJs = cmJs || {};
+
+cmJs.work = (function() {
+  
+  var init = function() {
+    setupFancybox();
+  },
+
+  setupFancybox = function() {
+    $(".fancybox").fancybox({
+      helpers : {
+        title : {
+          type : 'inside'
+        }
+      }
+    });
+    $('.fancybox-media').fancybox({
+      openEffect  : 'none',
+      closeEffect : 'none',
+      helpers : {
+        media : {},
+        title : {
+          type : 'inside'
+        }
+      }
+    });
   }
-});
-$('.fancybox-media').fancybox({
-  openEffect  : 'none',
-  closeEffect : 'none',
-  helpers : {
-    media : {},
-    title : {
-      type : 'inside'
-    }
-  }
-});
+
+  $(init);
+
+  return {
+    init: function() { $(init); }
+  };
+}(jQuery));
