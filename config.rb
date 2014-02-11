947932dd4869923111ce9de7f9f2b2f1c10fabc3
Craig Morey <craig.morey@uswitch.com>
2014-02-11 00:43:37 +0000

    get ready for deploy to live

--- config.rb
+++ config.rb
@@ -61,7 +61,7 @@ configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
 
-  set :site_root, '/new/'
+  set :site_root, '/'
 
   # Minify Javascript on build
   activate :minify_javascript
